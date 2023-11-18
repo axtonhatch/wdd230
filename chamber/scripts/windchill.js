81c39164a9e24d049f6c16db6750c0d4b38d0099
axtonhatch <axtonhatch2000@gmail.com>
2023-11-18 10:55:53 -0700

    yeppers

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,20 +1,52 @@
+// DOM elements
 const temperatureElement = document.getElementById('temperature');
-const windspeedElement = document.getElementById('windspeed');
-const windchillElement = document.getElementById('windchill');
+const weatherDescriptionElement = document.getElementById('weather-description');
+const threeDayForecastElement = document.getElementById('three-day-forecast');
+const meetGreetBanner = document.getElementById('meetGreetBanner');
 
-function calculateWindChill(temperature, windspeed) {
-  if (temperature <= 50 && windspeed > 3.0) {
-    const windChill = 35.74 + (0.6215 * temperature) - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
-    return windChill.toFixed(2);
-  } else {
-    return "N/A";
-  }
+// Fetch weather data from OpenWeatherMap API
+function fetchWeatherData() {
+  const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=40.234&lon=-111.659&units=imperial&appid=1ee773b191e6894fe565bfd075600b87`;
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=40.234&lon=-111.659&units=imperial&appid=1ee773b191e6894fe565bfd075600b87`;
+
+  // Fetch current weather data
+  fetch(currentWeatherURL)
+    .then(response => response.json())
+    .then(data => {
+      const currentTemperature = data.main.temp;
+      const weatherDescription = data.weather[0].description;
+
+      temperatureElement.textContent = `Temperature: ${currentTemperature}°C`;
+      weatherDescriptionElement.textContent = `Weather Description: ${weatherDescription}`;
+    })
+    .catch(error => console.error('Error fetching current weather:', error));
+
+  // Fetch three-day forecast
+  fetch(forecastURL)
+    .then(response => response.json())
+    .then(data => {
+      const forecast = data.list.slice(0, 8); // Select the first 8 entries (3 days with 3-hour intervals)
+
+      const threeDayTemperatures = forecast.map(entry => entry.main.temp);
+      const minTemperature = Math.min(...threeDayTemperatures);
+      const maxTemperature = Math.max(...threeDayTemperatures);
+
+      threeDayForecastElement.textContent = `Three-day Forecast: Min ${minTemperature}°C, Max ${maxTemperature}°C`;
+    })
+    .catch(error => console.error('Error fetching forecast:', error));
 }
 
-window.addEventListener('load', () => {
-  const temperature = parseFloat(temperatureElement.textContent);
-  const windspeed = parseFloat(windspeedElement.textContent);
+// Check if it's Monday, Tuesday, or Wednesday
+const today = new Date().getDay();
+if (today >= 1 && today <= 3) {
+  meetGreetBanner.style.display = 'block';
+}
 
-  const windChillValue = calculateWindChill(temperature, windspeed);
-  windchillElement.textContent = windChillValue;
-});
+
+function closeBanner() {
+meetGreetBanner.style.display = 'none';
+}
+
+
+// Call the function when the page loads
+window.addEventListener('load', fetchWeatherData);
